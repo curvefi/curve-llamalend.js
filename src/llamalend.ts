74f03e9c70e00ecc6136bf0871b05657d8a6b1d7
fedorovdg <fedorovdgap@gmail.com>
2025-06-24 01:11:28 +0400

    feat: add new leverage zap fro new mintMarket

--- src/llamalend.ts
+++ src/llamalend.ts
@@ -309,7 +309,7 @@ class Llamalend implements ILlamalend {
 
         const network = await this.provider.getNetwork();
         this.chainId = Number(network.chainId) === 133 || Number(network.chainId) === 31337 ? 1 : Number(network.chainId) as IChainId;
-        console.log("CURVE-LENDING-JS IS CONNECTED TO NETWORK:", { name: network.name.toUpperCase(), chainId: Number(this.chainId) });
+        console.log("CURVE-LLAMALEND-JS IS CONNECTED TO NETWORK:", { name: network.name.toUpperCase(), chainId: Number(this.chainId) });
 
         if(this.chainId === 42161) {
             this.constantOptions = { gasLimit: 1125899906842624 } // https://arbiscan.io/chart/gaslimit
@@ -455,7 +455,7 @@ class Llamalend implements ILlamalend {
                         controller_address: controllers[i],
                         monetary_policy_address,
                         collateral_address: is_eth ? "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" : collaterals[i],
-                        leverage_zap: "0x0000000000000000000000000000000000000000",
+                        leverage_zap: this.constants.ALIASES.leverage_zap,
                         deleverage_zap: "0x0000000000000000000000000000000000000000",
                         collateral_symbol: is_eth ? "ETH" : collateral_symbol,
                         collateral_decimals,
---
+++ src/mintMarkets/interfaces/leverage.ts
@@ -0,0 +1,104 @@
+import {IDict, TAmount, TGas} from "../../interfaces";
+
+export interface ILeverageV2 {
+    hasLeverage: () => boolean,
+
+    maxLeverage: (N: number) => Promise<string>,
+
+    createLoanMaxRecv: (userCollateral: TAmount, userBorrowed: TAmount, range: number) =>
+        Promise<{
+            maxDebt: string,
+            maxTotalCollateral: string,
+            userCollateral: string,
+            collateralFromUserBorrowed: string,
+            collateralFromMaxDebt: string,
+            maxLeverage: string,
+            avgPrice: string,
+        }>,
+    createLoanMaxRecvAllRanges: (userCollateral: TAmount, userBorrowed: TAmount) =>
+        Promise<IDict<{
+            maxDebt: string,
+            maxTotalCollateral: string,
+            userCollateral: string,
+            collateralFromUserBorrowed: string,
+            collateralFromMaxDebt: string,
+            maxLeverage: string,
+            avgPrice: string,
+        }>>,
+    createLoanExpectedCollateral: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, slippage?: number) =>
+        Promise<{
+            totalCollateral: string,
+            userCollateral: string,
+            collateralFromUserBorrowed: string,
+            collateralFromDebt: string,
+            leverage: string,
+            avgPrice: string
+        }>,
+    createLoanPriceImpact: (userBorrowed: TAmount, debt: TAmount) => Promise<string>,
+    createLoanMaxRange: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount) => Promise<number>,
+    createLoanBands: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, range: number) => Promise<[number, number]>,
+    createLoanBandsAllRanges: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount) => Promise<IDict<[number, number] | null>>,
+    createLoanPrices: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, range: number) => Promise<string[]>,
+    createLoanPricesAllRanges: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount) => Promise<IDict<[string, string] | null>>,
+    createLoanHealth: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, range: number, full?: boolean) => Promise<string>,
+    createLoanIsApproved: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<boolean>,
+    createLoanApprove: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<string[]>,
+    createLoanRouteImage: (userBorrowed: TAmount, debt: TAmount) => Promise<string>,
+    createLoan: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, range: number, slippage?: number) => Promise<string>,
+
+    borrowMoreMaxRecv: (userCollateral: TAmount, userBorrowed: TAmount, address?: string) =>
+        Promise<{
+            maxDebt: string,
+            maxTotalCollateral: string,
+            userCollateral: string,
+            collateralFromUserBorrowed: string,
+            collateralFromMaxDebt: string,
+            avgPrice: string,
+        }>,
+    borrowMoreExpectedCollateral: (userCollateral: TAmount, userBorrowed: TAmount, dDebt: TAmount, slippage?: number, address?: string) =>
+        Promise<{
+            totalCollateral: string,
+            userCollateral: string,
+            collateralFromUserBorrowed: string,
+            collateralFromDebt: string,
+            avgPrice: string
+        }>,
+    borrowMorePriceImpact: (userBorrowed: TAmount, dDebt: TAmount, address?: string) => Promise<string>,
+    borrowMoreBands: (userCollateral: TAmount, userBorrowed: TAmount, dDebt: TAmount, address?: string) => Promise<[number, number]>,
+    borrowMorePrices: (userCollateral: TAmount, userBorrowed: TAmount, dDebt: TAmount, address?: string) => Promise<string[]>,
+    borrowMoreHealth: (userCollateral: TAmount, userBorrowed: TAmount, dDebt: TAmount, full?: boolean, address?: string) => Promise<string>,
+    borrowMoreIsApproved: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<boolean>,
+    borrowMoreApprove: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<string[]>,
+    borrowMoreRouteImage: (userBorrowed: TAmount, debt: TAmount) => Promise<string>,
+    borrowMore: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, slippage?: number) => Promise<string>,
+
+    repayExpectedBorrowed: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, slippage?: number) =>
+        Promise<{
+            totalBorrowed: string,
+            borrowedFromStateCollateral: string,
+            borrowedFromUserCollateral: string,
+            userBorrowed: string,
+            avgPrice: string
+        }>,
+    repayPriceImpact: (stateCollateral: TAmount, userCollateral: TAmount) => Promise<string>,
+    repayIsFull: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, address?: string) => Promise<boolean>,
+    repayIsAvailable: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, address?: string) => Promise<boolean>,
+    repayBands: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, address?: string) => Promise<[number, number]>,
+    repayPrices: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, address?: string) => Promise<string[]>,
+    repayHealth: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, full?: boolean, address?: string) => Promise<string>,
+    repayIsApproved: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<boolean>,
+    repayApprove: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<string[]>,
+    repayRouteImage: (stateCollateral: TAmount, userCollateral: TAmount) => Promise<string>,
+    repay: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, slippage?: number) => Promise<string>,
+
+    estimateGas: {
+        createLoanApprove: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<TGas>,
+        createLoan: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, range: number, slippage?: number) => Promise<number>,
+
+        borrowMoreApprove: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<TGas>,
+        borrowMore: (userCollateral: TAmount, userBorrowed: TAmount, debt: TAmount, slippage?: number) => Promise<number>,
+
+        repayApprove: (userCollateral: TAmount, userBorrowed: TAmount) => Promise<TGas>,
+        repay: (stateCollateral: TAmount, userCollateral: TAmount, userBorrowed: TAmount, slippage?: number) => Promise<number>,
+    }
+}
