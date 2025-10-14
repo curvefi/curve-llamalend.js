a6f81a0b3be4f7a199b58947558117c4869228f5
fedorovdg <fedorovdgap@gmail.com>
2025-10-14 17:17:23 +0400

    chore: remove old _getUsdPricesFromApi

--- src/external-api.ts
+++ src/external-api.ts
@@ -6,7 +6,6 @@ import {
     IExtendedPoolDataFromApi,
     IMarketData,
     INetworkName,
-    IPoolFactory,
     IQuoteOdos,
     IResponseApi,
     IPoolType,
@@ -96,109 +95,6 @@ export const createUsdPricesDict = (allTypesExtendedPoolData:  IExtendedPoolData
     return priceDictByMaxTvl
 }
 
-
-const _getPoolsFromApi = memoize(
-    async (network: INetworkName, poolFactory: IPoolFactory ): Promise<IExtendedPoolDataFromApi> => {
-        const response = await fetch(`https://api.curve.finance/api/getPools/${network}/${poolFactory}`);
-        const { data } = await response.json() as { data?: IExtendedPoolDataFromApi, success: boolean };
-        return data ?? { poolData: [], tvl: 0, tvlAll: 0 };
-    },
-    {
-        promise: true,
-        maxAge: 5 * 60 * 1000, // 5m
-    }
-)
-
-const _getAllPoolsFromApi = async (network: INetworkName): Promise<IExtendedPoolDataFromApi[]> => {
-    return await Promise.all([
-        _getPoolsFromApi(network, "main"),
-        _getPoolsFromApi(network, "crypto"),
-        _getPoolsFromApi(network, "factory"),
-        _getPoolsFromApi(network, "factory-crvusd"),
-        _getPoolsFromApi(network, "factory-crypto"),
-        _getPoolsFromApi(network, "factory-twocrypto"),
-        _getPoolsFromApi(network, "factory-tricrypto"),
-        _getPoolsFromApi(network, "factory-stable-ng"),
-    ]);
-}
-
-export async function _getUsdPricesFromApi(this: Llamalend): Promise<IDict<number>> {
-    const network = this.constants.NETWORK_NAME;
-    const allTypesExtendedPoolData = await _getAllPoolsFromApi(network);
-    const priceDict: IDict<Record<string, number>[]> = {};
-    const priceDictByMaxTvl: IDict<number> = {};
-
-    for (const extendedPoolData of allTypesExtendedPoolData) {
-        for (const pool of extendedPoolData.poolData) {
-            const lpTokenAddress = pool.lpTokenAddress ?? pool.address;
-            const totalSupply = pool.totalSupply / (10 ** 18);
-            if(lpTokenAddress.toLowerCase() in priceDict) {
-                priceDict[lpTokenAddress.toLowerCase()].push({
-                    price: pool.usdTotal && totalSupply ? pool.usdTotal / totalSupply : 0,
-                    tvl: pool.usdTotal,
-                })
-            } else {
-                priceDict[lpTokenAddress.toLowerCase()] = []
-                priceDict[lpTokenAddress.toLowerCase()].push({
-                    price: pool.usdTotal && totalSupply ? pool.usdTotal / totalSupply : 0,
-                    tvl: pool.usdTotal,
-                })
-            }
-
-            for (const coin of pool.coins) {
-                if (typeof coin.usdPrice === "number") {
-                    if(coin.address.toLowerCase() in priceDict) {
-                        priceDict[coin.address.toLowerCase()].push({
-                            price: coin.usdPrice,
-                            tvl: pool.usdTotal,
-                        })
-                    } else {
-                        priceDict[coin.address.toLowerCase()] = []
-                        priceDict[coin.address.toLowerCase()].push({
-                            price: coin.usdPrice,
-                            tvl: pool.usdTotal,
-                        })
-                    }
-                }
-            }
-
-            for (const coin of pool.gaugeRewards ?? []) {
-                if (typeof coin.tokenPrice === "number") {
-                    if(coin.tokenAddress.toLowerCase() in priceDict) {
-                        priceDict[coin.tokenAddress.toLowerCase()].push({
-                            price: coin.tokenPrice,
-                            tvl: pool.usdTotal,
-                        });
-                    } else {
-                        priceDict[coin.tokenAddress.toLowerCase()] = []
-                        priceDict[coin.tokenAddress.toLowerCase()].push({
-                            price: coin.tokenPrice,
-                            tvl: pool.usdTotal,
-                        });
-                    }
-                }
-            }
-        }
-    }
-
-    for(const address in priceDict) {
-        if(priceDict[address].length > 0) {
-            const maxTvlItem = priceDict[address].reduce((prev, current) => {
-                if (+current.tvl > +prev.tvl) {
-                    return current;
-                } else {
-                    return prev;
-                }
-            });
-            priceDictByMaxTvl[address] = maxTvlItem.price
-        } else {
-            priceDictByMaxTvl[address] = 0
-        }
-
-    }
-
-    return priceDictByMaxTvl
-}
 
 type UserCollateral = { total_deposit_precise: string, total_deposit_from_user: number, total_deposit_usd_value: number , total_borrowed: number, total_deposit_from_user_precise: number, total_deposit_from_user_usd_value: number}
 export const _getUserCollateral = memoize(
