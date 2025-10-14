afeab007207c055b38df2a810c709658dee81f49
fedorovdg <fedorovdgap@gmail.com>
2025-10-14 17:08:16 +0400

    perf: update usdRate performance

---
+++ src/cached.ts
@@ -0,0 +1,82 @@
+import {IDict, IExtendedPoolDataFromApi, INetworkName, IPoolType} from "./interfaces.js";
+import {createUsdPricesDict, uncached_getAllPoolsFromApi} from './external-api.js'
+
+/**
+ * Memoizes a function that returns a promise.
+ * Custom function instead of `memoizee` because we want to be able to set the cache manually based on server data.
+ * @param fn The function that returns a promise and will be memoized
+ * @param maxAge The maximum age of the cache in milliseconds
+ * @param createKey A function that creates a key for the cache based on the arguments passed to the function
+ * @returns A memoized `fn` function that includes a `set` method to set the cache manually
+ */
+const memoize = <TResult, TParams extends any[], TFunc extends (...args: TParams) => Promise<TResult>>(fn: TFunc, {
+    maxAge,
+    createKey = (list) => list.toString(),
+}: {
+    maxAge: number,
+    createKey?: (args: TParams) => string
+}) => {
+    const cache: Record<string, Promise<TResult>> = {};
+    const timeouts: Record<string, ReturnType<typeof setTimeout>> = {};
+
+    const setCache = (key: string, promise?: Promise<TResult>) => {
+        if (promise) {
+            cache[key] = promise;
+        } else if (key in cache) {
+            delete cache[key];
+        }
+        if (key in timeouts) {
+            clearTimeout(timeouts[key]);
+            delete timeouts[key]
+        }
+    };
+
+    const scheduleCleanup = (key: string) => timeouts[key] = setTimeout(() => {
+        delete timeouts[key];
+        delete cache[key];
+    }, maxAge);
+
+    const cachedFn = async (...args: TParams): Promise<TResult> => {
+        const key = createKey(args);
+        if (key in cache) {
+            return cache[key];
+        }
+        const promise = fn(...args);
+        setCache(key, promise);
+        try {
+            const result = await promise;
+            scheduleCleanup(key)
+            return result;
+        } catch (e) {
+            delete cache[key];
+            throw e;
+        }
+    };
+
+    cachedFn.set = (result: TResult, ...args: TParams) => {
+        const key = createKey(args);
+        setCache(key, Promise.resolve(result));
+        scheduleCleanup(key);
+    }
+
+    return cachedFn as TFunc & { set: (result: TResult, ...args: TParams) => void };
+}
+
+const createCache = (poolsDict: Record<IPoolType, IExtendedPoolDataFromApi>) => {
+    const poolLists = Object.values(poolsDict)
+    const usdPrices = createUsdPricesDict(poolLists);
+    return {poolsDict, poolLists, usdPrices};
+};
+
+/**
+ * This function is used to cache the data fetched from the API and the data derived from it.
+ * Note: do not expose this function to the outside world, instead encapsulate it in a function that returns the data you need.
+ */
+const _getCachedData = memoize(async (network: INetworkName) =>
+    createCache(await uncached_getAllPoolsFromApi(network)), {maxAge: 1000 * 60 * 5 /* 5 minutes */})
+
+export const _getUsdPricesFromApi = async (network:INetworkName): Promise<IDict<number>> => {
+    const {usdPrices} = await _getCachedData(network);
+    return usdPrices
+}
+
--- src/external-api.ts
+++ src/external-api.ts
@@ -9,7 +9,92 @@ import {
     IPoolFactory,
     IQuoteOdos,
     IResponseApi,
+    IPoolType,
 } from "./interfaces";
+
+const uncached_getPoolsFromApi = async (network: INetworkName, poolType: IPoolType): Promise<IExtendedPoolDataFromApi> => {
+    const api = "https://api.curve.finance/api";
+    const url = `${api}/getPools/${network}/${poolType}`;
+    return await fetchData(url) ?? { poolData: [], tvl: 0, tvlAll: 0 };
+}
+
+const getPoolTypes = () => ["main", "crypto", "factory", "factory-crvusd", "factory-crypto", "factory-twocrypto", "factory-tricrypto", "factory-stable-ng"] as const;
+export const uncached_getAllPoolsFromApi = async (network: INetworkName): Promise<Record<IPoolType, IExtendedPoolDataFromApi>> =>
+    Object.fromEntries(
+        await Promise.all(getPoolTypes().map(async (poolType) => {
+            const data = await uncached_getPoolsFromApi(network, poolType);
+            return [poolType, data];
+        }))
+    )
+
+export const createUsdPricesDict = (allTypesExtendedPoolData:  IExtendedPoolDataFromApi[]): IDict<number> => {
+    const priceDict: IDict<Record<string, number>[]> = {};
+    const priceDictByMaxTvl: IDict<number> = {};
+
+    for (const extendedPoolData of allTypesExtendedPoolData) {
+        for (const pool of extendedPoolData.poolData) {
+            const lpTokenAddress = pool.lpTokenAddress ?? pool.address;
+            const totalSupply = pool.totalSupply / (10 ** 18);
+            if(lpTokenAddress.toLowerCase() in priceDict) {
+                priceDict[lpTokenAddress.toLowerCase()].push({
+                    price: pool.usdTotal && totalSupply ? pool.usdTotal / totalSupply : 0,
+                    tvl: pool.usdTotal,
+                })
+            } else {
+                priceDict[lpTokenAddress.toLowerCase()] = []
+                priceDict[lpTokenAddress.toLowerCase()].push({
+                    price: pool.usdTotal && totalSupply ? pool.usdTotal / totalSupply : 0,
+                    tvl: pool.usdTotal,
+                })
+            }
+
+            for (const coin of pool.coins) {
+                if (typeof coin.usdPrice === "number") {
+                    if(coin.address.toLowerCase() in priceDict) {
+                        priceDict[coin.address.toLowerCase()].push({
+                            price: coin.usdPrice,
+                            tvl: pool.usdTotal,
+                        })
+                    } else {
+                        priceDict[coin.address.toLowerCase()] = []
+                        priceDict[coin.address.toLowerCase()].push({
+                            price: coin.usdPrice,
+                            tvl: pool.usdTotal,
+                        })
+                    }
+                }
+            }
+
+            for (const coin of pool.gaugeRewards ?? []) {
+                if (typeof coin.tokenPrice === "number") {
+                    if(coin.tokenAddress.toLowerCase() in priceDict) {
+                        priceDict[coin.tokenAddress.toLowerCase()].push({
+                            price: coin.tokenPrice,
+                            tvl: pool.usdTotal,
+                        });
+                    } else {
+                        priceDict[coin.tokenAddress.toLowerCase()] = []
+                        priceDict[coin.tokenAddress.toLowerCase()].push({
+                            price: coin.tokenPrice,
+                            tvl: pool.usdTotal,
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    for(const address in priceDict) {
+        if (priceDict[address].length) {
+            const maxTvlItem = priceDict[address].reduce((prev, current) => +current.tvl > +prev.tvl ? current : prev);
+            priceDictByMaxTvl[address] = maxTvlItem.price
+        } else {
+            priceDictByMaxTvl[address] = 0
+        }
+    }
+
+    return priceDictByMaxTvl
+}
 
 
 const _getPoolsFromApi = memoize(
@@ -232,3 +317,13 @@ export const _getHiddenPools = memoize(
         maxAge: 5 * 60 * 1000, // 5m
     }
 )
+
+async function fetchJson(url: string): Promise<any> {
+    const response = await fetch(url);
+    return await response.json() ?? {};
+}
+
+async function fetchData(url: string) {
+    const {data} = await fetchJson(url);
+    return data;
+}
