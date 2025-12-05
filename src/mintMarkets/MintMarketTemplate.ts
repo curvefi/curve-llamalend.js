786defa36623f08e751fad98f40e6375ec112d7f
fedorovdg <fedorovdgap@gmail.com>
2025-12-05 21:47:05 +0400

    feat: add future leverage for add and remove collateral

--- src/mintMarkets/MintMarketTemplate.ts
+++ src/mintMarkets/MintMarketTemplate.ts
@@ -1001,6 +1001,21 @@ export class MintMarketTemplate {
         return await this._addCollateral(collateral, address, false) as string;
     }
 
+    public async addCollateralFutureLeverage(collateral: number | string, userAddress = ''): Promise<string> {
+        this._checkLeverageForStats();
+        userAddress = _getAddress.call(this.llamalend, userAddress);
+        const [userCollateral, {collateral: currentCollateral}] = await Promise.all([
+            _getUserCollateralCrvUsdFull(this.llamalend.constants.NETWORK_NAME, this.controller, userAddress),
+            this.userState(userAddress),
+        ]);
+
+        const total_deposit_from_user = userCollateral.total_deposit_from_user_precise ?? userCollateral.total_deposit_precise;
+        const collateralBN = BN(collateral);
+        const currentCollateralBN = BN(currentCollateral);
+
+        return currentCollateralBN.plus(collateralBN).div(BN(total_deposit_from_user).plus(collateralBN)).toString();
+    }
+
     // ---------------- REMOVE COLLATERAL ----------------
 
     public async maxRemovable(): Promise<string> {
@@ -1067,6 +1082,21 @@ export class MintMarketTemplate {
 
     public async removeCollateral(collateral: number | string): Promise<string> {
         return await this._removeCollateral(collateral, false) as string;
+    }
+
+    public async removeCollateralFutureLeverage(collateral: number | string, userAddress = ''): Promise<string> {
+        this._checkLeverageForStats();
+        userAddress = _getAddress.call(this.llamalend, userAddress);
+        const [userCollateral, {collateral: currentCollateral}] = await Promise.all([
+            _getUserCollateralCrvUsdFull(this.llamalend.constants.NETWORK_NAME, this.controller, userAddress),
+            this.userState(userAddress),
+        ]);
+
+        const total_deposit_from_user = userCollateral.total_deposit_from_user_precise ?? userCollateral.total_deposit_precise;
+        const collateralBN = BN(collateral);
+        const currentCollateralBN = BN(currentCollateral);
+
+        return currentCollateralBN.minus(collateralBN).div(BN(total_deposit_from_user).minus(collateralBN)).toString();
     }
 
     // ---------------- REPAY ----------------
