5e49fc9607eb0bde09f11fc0513d361726a0ca0e
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-05-06 14:13:19 +0200

    refactor: parallelize fetchStats

--- src/lendMarkets/fetch/fetchLendMarkets.ts
+++ src/lendMarkets/fetch/fetchLendMarkets.ts
@@ -9,6 +9,7 @@ import MonetaryPolicyABI from '../../constants/abis/MonetaryPolicy.json' with {t
 import VaultABI from '../../constants/abis/Vault.json' with {type: 'json'};
 import GaugeABI from '../../constants/abis/GaugeV5.json' with {type: 'json'};
 import SidechainGaugeABI from '../../constants/abis/SidechainGauge.json' with {type: 'json'};
+import ERC20ABI from '../../constants/abis/ERC20.json' with {type: 'json'};
 
 const controllerAbiMap = {
     'v1' : ControllerABI,
@@ -26,6 +27,26 @@ type FactoryData = {
     gauges: string[],
 };
 
+const registerContracts = (
+    llamalend: Llamalend,
+    { amms, controllers, borrowed_tokens, collateral_tokens, monetary_policies, vaults, gauges }: FactoryData,
+    version: 'v1' | 'v2'
+) => {
+    amms.forEach((_, index) => {
+        llamalend.setContract(amms[index], LlammaABI);
+        llamalend.setContract(controllers[index], controllerAbiMap[version]);
+        llamalend.setContract(monetary_policies[index], MonetaryPolicyABI);
+        llamalend.setContract(vaults[index], VaultABI);
+        llamalend.setContract(borrowed_tokens[index], ERC20ABI);
+        llamalend.setContract(collateral_tokens[index], ERC20ABI);
+        const gauge = gauges[index];
+        if (gauge && gauge !== llamalend.constants.ZERO_ADDRESS) {
+            llamalend.setContract(gauge, llamalend.chainId === 1 ? GaugeABI : SidechainGaugeABI);
+        }
+    });
+};
+
+
 const registerMarkets = (
     llamalend: Llamalend,
     { names, amms, controllers, borrowed_tokens, collateral_tokens, monetary_policies, vaults, gauges }: FactoryData,
@@ -39,44 +60,32 @@ const registerMarkets = (
     }
 
     amms.forEach((_, index) => {
-        llamalend.setContract(amms[index], LlammaABI);
-        llamalend.setContract(controllers[index], controllerAbiMap[version]);
-        llamalend.setContract(monetary_policies[index], MonetaryPolicyABI);
-        llamalend.setContract(vaults[index], VaultABI);
-        if (gauges[index]) {
-            llamalend.setContract(gauges[index], llamalend.chainId === 1 ? GaugeABI : SidechainGaugeABI);
+        const gauge = gauges[index];
+        if (gauge && gauge !== llamalend.constants.ZERO_ADDRESS) {
+            llamalend.constants.DECIMALS[gauge] = 18;
         }
-        COIN_DATA[vaults[index]] = {
-            address: vaults[index],
-            decimals: 18,
-            name: "Curve Vault for " + COIN_DATA[borrowed_tokens[index]].name,
-            symbol: "cv" + COIN_DATA[borrowed_tokens[index]].symbol,
-        };
-        COIN_DATA[gauges[index]] = {
-            address: gauges[index],
-            decimals: 18,
-            name: "curve.finance " + COIN_DATA[borrowed_tokens[index]].name + " Gauge Deposit",
-            symbol: "cv" + COIN_DATA[borrowed_tokens[index]].symbol + "-gauge",
-        };
-        llamalend.constants.DECIMALS[vaults[index]] = 18;
-        llamalend.constants.DECIMALS[gauges[index]] = 18;
+        const vault = vaults[index];
+        const borrowed_token = borrowed_tokens[index];
+        const collateral_token = collateral_tokens[index];
+        llamalend.constants.DECIMALS[vault] = 18;
+
         const marketId = { v1: `one-way-market-${index}`, v2: `one-way-market-v2-${index}` }[version];
         if (hidden.has(marketId)) return;
 
         const marketData = {
-            name: names[index] || `${COIN_DATA[collateral_tokens[index]].symbol}/${COIN_DATA[borrowed_tokens[index]].symbol}`,
+            name: names[index] || `${COIN_DATA[collateral_token].symbol}/${COIN_DATA[borrowed_token].symbol}`,
             version: version,
             addresses: {
                 amm: amms[index],
                 controller: controllers[index],
-                borrowed_token: borrowed_tokens[index],
-                collateral_token: collateral_tokens[index],
+                borrowed_token,
+                collateral_token,
                 monetary_policy: monetary_policies[index],
-                vault: vaults[index],
-                gauge: gauges[index],
+                vault,
+                gauge,
             },
-            borrowed_token: COIN_DATA[borrowed_tokens[index]],
-            collateral_token: COIN_DATA[collateral_tokens[index]],
+            borrowed_token: COIN_DATA[borrowed_token],
+            collateral_token: COIN_DATA[collateral_token],
         };
 
         const constant = { v1: 'ONE_WAY_MARKETS' as const, v2: 'ONE_WAY_MARKETS_V2' as const }[version];
@@ -87,21 +96,22 @@ const registerMarkets = (
 export const fetchOneWayMarketsByBlockchain = async (llamalend: Llamalend, version: 'v1' | 'v2' = 'v1') => {
     const hiddenMarketsPromise = llamalend._getHiddenMarkets();
     const factoryData = (await (version === 'v2' ? getFactoryMarketDataV2 :  getFactoryMarketDataV1)(llamalend)) as FactoryData
+    registerContracts(llamalend, factoryData, version)
 
     const { amms, vaults, controllers, borrowed_tokens, collateral_tokens } = factoryData;
     const [coins, hiddenMarkets] = await Promise.all([
         llamalend.getCoins(collateral_tokens, borrowed_tokens),
         hiddenMarketsPromise,
+        llamalend.fetchStats(amms, controllers, vaults, borrowed_tokens, collateral_tokens, version),
     ])
 
     registerMarkets(llamalend, factoryData, coins, version, hiddenMarkets);
-
-    await llamalend.fetchStats(amms, controllers, vaults, borrowed_tokens, collateral_tokens, version);
 };
 
 export const fetchOneWayMarketsByAPI = async (llamalend: Llamalend, version: 'v1' | 'v2' = 'v1') => {
     const hiddenMarketsPromise = llamalend._getHiddenMarkets();
     const factoryData = (await getFactoryMarketDataByAPI(llamalend)) as FactoryData;
+    registerContracts(llamalend, factoryData, version)
     const [coins, hiddenMarkets] = await Promise.all([
         llamalend.getCoins(factoryData.collateral_tokens, factoryData.borrowed_tokens, true),
         hiddenMarketsPromise,
