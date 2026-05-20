86502bcda113ddfc202d46b6288afd633cc77709
fedorovdg <fedorovdgap@gmail.com>
2026-05-20 08:47:33 +0300

    fix: fixed leverageZap for mintMarkets

--- src/mintMarkets/setupContracts.ts
+++ src/mintMarkets/setupContracts.ts
@@ -8,15 +8,16 @@ import controllerV2ABI from "../constants/abis/crvUSD/controller_v2.json" with {
 import LeverageZapCrvUSDABI from "../constants/abis/crvUSD/LeverageZap.json" with { type: "json" };
 import DeleverageZapABI from "../constants/abis/crvUSD/DeleverageZap.json" with { type: "json" };
 import HealthCalculatorZapABI from "../constants/abis/crvUSD/HealthCalculatorZap.json" with { type: "json" };
+import LeverageZapABI from "../constants/abis/LeverageZap.json" with { type: "json" };
 import { resolveMonetaryPolicyAbi } from "./monetaryPolicyAbi.js";
 
 const NATIVE_ETH_PLACEHOLDER = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 
 export const setupMintMarketContracts = (llamalend: Llamalend, llammaData: ILlamma): void => {
-    const isV2Controller = llammaData.is_deleverage_supported === true;
+    const isNewMarket = llammaData.is_deleverage_supported === true;
 
     llamalend.setContract(llammaData.amm_address, llammaABI);
-    llamalend.setContract(llammaData.controller_address, isV2Controller ? controllerV2ABI : controllerABI);
+    llamalend.setContract(llammaData.controller_address, isNewMarket ? controllerV2ABI : controllerABI);
     llamalend.setContract(llammaData.monetary_policy_address, resolveMonetaryPolicyAbi(llammaData.monetary_policy_address));
 
     if (llammaData.collateral_address === NATIVE_ETH_PLACEHOLDER) {
@@ -25,7 +26,7 @@ export const setupMintMarketContracts = (llamalend: Llamalend, llammaData: ILlam
         llamalend.setContract(llammaData.collateral_address, ERC20ABI);
     }
 
-    llamalend.setContract(llammaData.leverage_zap, LeverageZapCrvUSDABI);
+    llamalend.setContract(llammaData.leverage_zap, isNewMarket ?  LeverageZapABI : LeverageZapCrvUSDABI);
     llamalend.setContract(llammaData.deleverage_zap, DeleverageZapABI);
     if (llammaData.health_calculator_zap && llammaData.health_calculator_zap !== llamalend.constants.ZERO_ADDRESS) {
         llamalend.setContract(llammaData.health_calculator_zap, HealthCalculatorZapABI);
