5f40fa2eba5a2f68a323545c40cd8d9d218fc5da
fedorovdg <fedorovdgap@gmail.com>
2026-08-03 18:10:01 +0300

    fix: fixed future-rate for llv2

--- src/lendMarkets/modules/common/statsBase.ts
+++ src/lendMarkets/modules/common/statsBase.ts
@@ -72,15 +72,22 @@ export class StatsBaseModule {
 
     private _getFutureRate = async (_dReserves: bigint, _dDebt: bigint): Promise<bigint> => {
         const mpContract = this.llamalend.contracts[this.market.addresses.monetary_policy].contract;
+
         const futureRateCall = mpContract.interface.hasFunction('future_rate(address,int256,int256)')
             ? mpContract.future_rate(this.market.addresses.controller, _dReserves, _dDebt)
             : mpContract.future_rate(_dReserves, _dDebt);
+
+        if (_dDebt > BigInt(0)) {
+            return futureRateCall;
+        }
+
         const [_rate, _adminPercentage] = await Promise.all([
             futureRateCall,
             this._fetchAdminPercentage(),
         ]);
+
         return _rate * (PRECISION - _adminPercentage) / PRECISION;
-    }
+    };
 
     public statsParameters = memoize(async (): Promise<{
         fee: string, // %
